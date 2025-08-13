01f5a5ccc39cd409099c4909b8c6ea1bbbb01ec4
JoaoCrv <52920332+JoaoCrv@users.noreply.github.com>
2025-08-13 21:19:45 +0100

    Chore: add method for autoupdate according to oficial documentation

--- Aquila/App.xaml.cs
+++ Aquila/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Velopack;
 
 namespace Aquila
 {
@@ -9,6 +10,30 @@ namespace Aquila
     /// </summary>
     public partial class App : Application
     {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            VelopackApp.Build().Run();
+            App app = new();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        private static async Task UpdateMyApp()
+        {
+            var mgr = new UpdateManager("https://the.place/you-host/updates");
+
+            // check for new version
+            var newVersion = await mgr.CheckForUpdatesAsync();
+            if (newVersion == null)
+                return; // no update available
+
+            // download new version
+            await mgr.DownloadUpdatesAsync(newVersion);
+
+            // install new version and restart app
+            mgr.ApplyUpdatesAndRestart(newVersion);
+        }
     }
 
 }
