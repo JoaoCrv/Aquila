ef6098f1ee68a915f0783cc53e965f79d878112f
JoaoCrv <52920332+JoaoCrv@users.noreply.github.com>
2025-11-22 20:24:50 +0000

    Revert "Refactor to MVVM and update project structure"
    This reverts commit 73da227873ed6ee0765be2b6f89a7f034164e779.

--- Aquila/Views/MainWindow.xaml.cs
+++ Aquila/Views/MainWindow.xaml.cs
@@ -1,18 +1,50 @@
-﻿
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using System.Windows;
-using Aquila.ViewModels;
+using System.Windows.Threading;
+using Aquila.Services.Utilities;
 
 namespace Aquila
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        
+        private readonly DispatcherTimer _timer;
+        private readonly HardwareMonitorService _monitor;
+
+        public ObservableCollection<SensorInfo> Sensores { get; set; } = [];
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
 
-            DataContext = new MainWindowViewModel();
+            _monitor = new HardwareMonitorService();
+            _monitor.StartMonitoring();
 
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            _timer.Tick += UpdateHardwareInfo;
+            _timer.Start();
+
+            //versionTextBlock.Text = Aquila.Services.Utilities.AppInfo.GetApplicationVersion();
         }
+
+        private void UpdateHardwareInfo(object? sender, EventArgs e)
+        {
+            var novos = _monitor.GetUpdatedSensorReadings();
+
+            Sensores.Clear();
+            foreach (var sensor in novos)
+                Sensores.Add(sensor);
+        }
+
+        // Suporte a INotifyPropertyChanged (para futura expansão)
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
