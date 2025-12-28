dc23e8c4d6ad6ace4a9e64a7c620db4b79089adf
JoaoCrv <52920332+JoaoCrv@users.noreply.github.com>
2025-12-28 21:25:35 +0000

    Enhance hardware monitoring for CPU, GPU, and RAM
    Added `DashboardViewModel` to include new properties for
    monitoring CPU, GPU, and RAM hardware data, such as names,
    temperatures, usage, fan speeds, power usage, and memory
    statistics. Replaced outdated GPU properties with updated
    paths and added new GPU-specific properties.
    
    Refactored `DashboardPage.xaml` to reflect the new
    `DashboardViewModel` properties. Improved layout with
    organized sections for CPU, GPU, and RAM data using
    `ui:Card` components. Enhanced design dimensions and
    bindings for better alignment and dynamic updates.

--- ViewModels/Pages/DashboardViewModel.cs
+++ ViewModels/Pages/DashboardViewModel.cs
@@ -10,6 +10,8 @@ namespace Aquila.ViewModels.Pages
         private readonly HardwareMonitorService _monitorService;
 
         public ComputerData Computer => _monitorService.ComputerData;
+
+        //CPU
         public string? CpuName => _monitorService.ComputerData.HardwareList
                                             .FirstOrDefault(h => h.HardwareType == LibreHardwareMonitor.Hardware.HardwareType.Cpu)?
                                             .Name;
@@ -20,14 +22,28 @@ namespace Aquila.ViewModels.Pages
         public DataSensor? CpuFanSpeed1Sensor => Computer.SensorIndex.GetValueOrDefault("/lpc/nct6687d/0/fan/0");
         public DataSensor? CpuFanSpeed2Sensor => Computer.SensorIndex.GetValueOrDefault("/lpc/nct6687d/0/fan/1");
 
-        public DataSensor? GpuUsageSensor => Computer.SensorIndex.GetValueOrDefault("/lpc/nct6687d/0/temperature/0");
-        public DataSensor? GpuTemperatureSensor => Computer.SensorIndex.GetValueOrDefault("/lpc/nct6687d/0/temperature/0");
-        public DataSensor? GpuSpeedSensor => Computer.SensorIndex.GetValueOrDefault("/lpc/nct6687d/0/temperature/0");
-        public DataSensor? GpuEnergySensor => Computer.SensorIndex.GetValueOrDefault("/lpc/nct6687d/0/temperature/0");
-        public DataSensor? GpuFanSpeed1Sensor => Computer.SensorIndex.GetValueOrDefault("/lpc/nct6687d/0/temperature/0");
-        public DataSensor? GpuFanSpeed2Sensor => Computer.SensorIndex.GetValueOrDefault("/lpc/nct6687d/0/temperature/0");
 
+        //GPU
+        public string? GpuName => _monitorService.ComputerData.HardwareList
+                                    .FirstOrDefault(h => h.HardwareType == LibreHardwareMonitor.Hardware.HardwareType.GpuAmd)?
+                                    .Name;
+        public DataSensor? GpuUsageSensor => Computer.SensorIndex.GetValueOrDefault("/gpu-amd/5/load/0");
+        public DataSensor? GpuTemperatureSensor => Computer.SensorIndex.GetValueOrDefault("/gpu-amd/5/temperature/0");
+        public DataSensor? GpuSpeedSensor => Computer.SensorIndex.GetValueOrDefault("/gpu-amd/5/clock/0");
+        public DataSensor? GpuEnergySensor => Computer.SensorIndex.GetValueOrDefault("/gpu-amd/5/power/3");
+        public DataSensor? GpuFanSpeed1Sensor => Computer.SensorIndex.GetValueOrDefault("/gpu-amd/5/fan/0");
+        public DataSensor? GpuFanSpeed2Sensor => Computer.SensorIndex.GetValueOrDefault("/gpu-amd/5/0/fan/1");
 
+        //RAM
+        public string? MemoryName => _monitorService.ComputerData.HardwareList
+                                    .FirstOrDefault(h => h.HardwareType == LibreHardwareMonitor.Hardware.HardwareType.Memory)?
+                                    .Name;
+        public DataSensor? MemoryUsageSensor => Computer.SensorIndex.GetValueOrDefault("/ram/load/0");
+        public DataSensor? MemoryAvailableSensor => Computer.SensorIndex.GetValueOrDefault("/ram/data/1");
+        public DataSensor? MemoryUsedSensor => Computer.SensorIndex.GetValueOrDefault("/ram/data/0");
+        public DataSensor? VirtualMemoryUsageSensor => Computer.SensorIndex.GetValueOrDefault("/ram/load/1");
+        public DataSensor? VirtualMemoryAvailableSensor => Computer.SensorIndex.GetValueOrDefault("/ram/data/3");
+        public DataSensor? VirtualMemoryUsedSensor => Computer.SensorIndex.GetValueOrDefault("/ram/data/2");
 
         public DashboardViewModel(HardwareMonitorService monitorService)
         {
@@ -36,12 +52,31 @@ namespace Aquila.ViewModels.Pages
             
             _monitorService.DataUpdated += () =>
             {
+                //CPU
                 OnPropertyChanged(nameof(CpuName));
                 OnPropertyChanged(nameof(CpuTemperatureSensor));
                 OnPropertyChanged(nameof(CpuUsageSensor));
                 OnPropertyChanged(nameof(CpuEnergySensor));
                 OnPropertyChanged(nameof(CpuFanSpeed1Sensor));
                 OnPropertyChanged(nameof(CpuFanSpeed2Sensor));
+
+                //Gpu
+                OnPropertyChanged(nameof(GpuName));
+                OnPropertyChanged(nameof(GpuTemperatureSensor));
+                OnPropertyChanged(nameof(GpuUsageSensor));
+                OnPropertyChanged(nameof(GpuEnergySensor));
+                OnPropertyChanged(nameof(GpuFanSpeed1Sensor));
+                OnPropertyChanged(nameof(GpuFanSpeed2Sensor));
+
+                //RAM
+                OnPropertyChanged(nameof(MemoryName));
+                OnPropertyChanged(nameof(MemoryUsageSensor));
+                OnPropertyChanged(nameof(MemoryAvailableSensor));
+                OnPropertyChanged(nameof(MemoryUsedSensor));
+                OnPropertyChanged(nameof(VirtualMemoryUsageSensor));
+                OnPropertyChanged(nameof(VirtualMemoryAvailableSensor));
+                OnPropertyChanged(nameof(VirtualMemoryUsedSensor));
+
 
             };
         }
